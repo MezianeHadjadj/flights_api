8b5d2ca2eeb3bc525f2ddbd2f5852d928b8852ba
Meziane HADJADJ <mezianeh3@gmail.com>
2016-03-05 00:22:36 +0100

    basic api schema

---
+++ apis/flights_apis.js
@@ -0,0 +1,12 @@
+module.exports = exports = function(app) {
+    app.get('/api/flights/list', exports.list)
+}
+
+
+
+exports.list =  function (req, res) {
+    //    if (!req.app.validate(req.body, 'flight_validate', res)) return
+    res.json({"flights":"results"});
+
+};
+
