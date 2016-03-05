8d743513a8425864adc852a7cabb377726f35ae0
Meziane HADJADJ <mezianeh3@gmail.com>
2016-03-05 21:57:12 +0100

    schema verification of form

--- public/js/controllers/flightsController.js
+++ public/js/controllers/flightsController.js
@@ -3,6 +3,7 @@ angular.module('flightsController', [])
     .controller('mainController', ['$scope','$http','Flights', function($scope, $http, Flights) {
 
         $scope.get_flights = function() {
+            console.log("request:"+$scope.request);
             $scope.flight_request = {
                 "start_date": "",
                 "return_date":"",
@@ -14,7 +15,7 @@ angular.module('flightsController', [])
                 "max_price_currency":""
             };
 
-            Flights.get_flights($scope.flight_request)
+            Flights.get_flights($scope.request)
                 .success(function(data) {
                     console.log(data)
                     $scope.results=JSON.parse(data)
